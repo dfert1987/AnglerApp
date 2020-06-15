bc73f67ef9ada780dcc456f47b30c331fbe21f2a
dfert1987 <github email address>
2020-06-15 13:45:43 -0600

    basic main options page added with correct background image

--- src/home.js
+++ src/home.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import Navbar from './navbar.js'
+import './home.css'
+
+
+
 
 class Home extends Component{
     render () {
     return  (
-    <container>
-    <div className="main">
-        <div className="nav-bar">
+    <div class="main-container">
+        <div className="NavBar">
+            <Navbar />
         </div>
-        <div>
-            <h1>WELCOME TO ANGLER</h1>
-        </div>  
+        <div className="main">
+            <div className="welcome-container">
+                <h1 className="welcome">WELCOME TO ANGLER</h1>
+            </div>
+            <div class="button-container">
+                <div class="button-card" id="button-tb">
+                    <img class="button-image" src="https://i.imgur.com/ppK33JO.jpg" alt="TackleBox"></img>
+                    <a class="link"  href="/tacklebox">
+                        <button id="tbbutton" class="btn">TackleBox</button>
+                    </a>
+                </div>
+                <div class="button-card" id="button-map">
+                    <img class="button-image" src="https://i.imgur.com/215fMsA.jpg" alt="Map"></img>
+                    <a class="link" href="/map">
+                        <button id="mapbutton" class="btn">Map</button>
+                    </a>
+                </div>
+                <div class="button-card" id="button-fish">
+                    <img class="button-image" src="https://i.imgur.com/z7FjF2V.jpg" alt="Fish"></img>
+                    <a class="link"  href="/fish">
+                        <button id="fishbutton" class="btn">Fish-dex</button>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-    </container>
-    )
+        )
     } 
 }
 export default Home;
--- src/title.js
+++ src/title.js
@@ -1,27 +1,23 @@
 import React, {Component} from 'react';
 import './title.css'
-import './App.css' 
 import Navbar from './navbar.js'
 import fishicon from './images/fish.png';
 
 class Title extends Component {
   render() {
   return  (
-    <div className="site-title">
+    <div className="landing-container">
       <Navbar />
-      <h3>Welcome to</h3>
-      <h1>ANGLER</h1>
-      <h4> - LOG EVERY CAST - </h4>
-
-        <section className="button-section">
-          <a href="/home">
-            <button className="enter-button">Enter </button>
-          </a>
-          <img className="fish" src={fishicon}/>
-          </section>
+      <h3 className="welcome-to">Welcome to</h3>
+      <h1 className="Title">ANGLER</h1>
+      <h4 className="subtitle"> - LOG EVERY CAST - </h4>
+      <section className="button-section">
+        <a href="/home">
+          <button className="enter-button">Enter </button>
+        </a>
+        <img className="fish" src={fishicon}/>
+      </section>
     </div>
-   
-
   )
 }}
 export default Title;
