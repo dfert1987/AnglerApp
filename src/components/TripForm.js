a8130f47c9a24fc91be766b51603e0a10db90f53
dfert1987 <davidj.fertitta@gmail.com>
2020-08-04 12:39:30 -0600

    datepicker added to trip form

--- src/components/TripForm.js
+++ src/components/TripForm.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react'
+import React, {Component } from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class TripForm extends Component {
+
+    state = {
+        startDate: new Date()
+    }
+
+    handleSelect = date => {
+        this.setState({
+            startDate: date
+        });
+    };
+
     render(){ 
         return (
-        <div className="formcontainer">
-            <form class="form">
+        <div className="formcont">
+            <form class="addtripform">
             <h2 className="log">LOG YOUR TRIP!</h2>
-            <p type="Date:"><input placeholder="When was your trip?"></input></p>
-            <p type="Time:"><input placeholder="What time were you fishing?"></input></p>
-            <p type="Weather:"><input placeholder="How was the weather?"></input></p>
-            <p type="Description:"><input placeholder="Describe your day fishing!"></input></p>
+            <DatePicker 
+                selected={this.state.startDate} 
+                onSelect={this.handleSelect}/>
+            <p type="Time:"><input placeholder="Start time?"></input></p>
+            <p type="Weather:"><input placeholder="Weather?"></input></p>
+            <p type="Description:"><input placeholder="Describe your day fishing..."></input></p>
             <button>Log It!</button>
             <div>
             </div>
