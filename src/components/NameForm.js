495501bee7b1a92a993ec91351c416c2dbe1ec42
dfert1987 <davidj.fertitta@gmail.com>
2020-08-21 12:01:47 -0600

    name form changes name state on submit

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -3,12 +3,27 @@ import Geocode from "react-geocode";
 
 class NameForm extends Component {
 
+    state = {
+        name: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]:event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
     render(){
         return(
             <div>
-                <form className="addMarkerFormName">
+                <form className="addMarkerFormName" onSubmit={this.handleSubmit}>
                     <label className="nameLabel">Location Name: </label>
-                    <input className="addressInput" type="text" placeholder="Lunker Lake"></input>
+                    <input className="addressInput" type="text" name="name" placeholder="Lunker Lake" onChange={this.handleChange}></input>
                     <input className="nameSubmit" type="submit"></input>
                 </form>
             </div>
