93b52246879440430d9a7a0780b574f9de9cb86b
dfert1987 <davidj.fertitta@gmail.com>
2020-09-01 07:38:24 -0600

    cleaned up code on trip form

--- src/components/TripForm.js
+++ src/components/TripForm.js
@@ -1,5 +1,4 @@
 import React, {Component } from 'react'
-import TargetedSpeciesDropDown from './TargetedSpeciesDropDown';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class TripForm extends Component {
@@ -19,8 +18,6 @@ export default class TripForm extends Component {
         targetedSpecies: 0
     }
 
-   
-
     handleChange = (event) => {
         this.setState({
             [event.target.name]:event.target.value
@@ -32,25 +29,25 @@ export default class TripForm extends Component {
         this.fetchFish()
     }
 
-        fetchFish = () => {
-            fetch('http://localhost:3000/fish')
-                .then(response => response.json())
-                .then(response => this.setFish(response)
-            )
-        }
+    fetchFish = () => {
+        fetch('http://localhost:3000/fish')
+        .then(response => response.json())
+        .then(response => this.setFish(response)
+        )
+    }
 
-        setFish = (response) => {
-            this.setState(
-                { allFish:response }
-            )
-        }
+    setFish = (response) => {
+        this.setState(
+            { allFish:response }
+        )
+    }
 
-        fetchLures = () => {
+    fetchLures = () => {
         fetch(`http://localhost:3000/lures`)
-            .then(response => response.json())
-            .then(response => this.setLures(response)
-            )
-        }
+        .then(response => response.json())
+        .then(response => this.setLures(response)
+        )
+    }
     
     setLures = (lures) => {
         this.setState(
@@ -61,7 +58,7 @@ export default class TripForm extends Component {
     
     setTackleBox = () => {
         const setTB = this.state.allLures.filter(lure => lure.favorited !== false)
-            this.setState(
+        this.setState(
             { tackleBox:setTB }
         )
     }
@@ -84,7 +81,6 @@ export default class TripForm extends Component {
         let lure_id = this.state.bestLure
         let location_id = this.props.location
         let fish_id = this.state.targetedSpecies
-
         let addedTrip = {
             duration, 
             time_start,
@@ -106,88 +102,88 @@ export default class TripForm extends Component {
                 <div className="formcont">
                     <h2 className="log">LOG YOUR TRIP!</h2>
                     <div className="top-row">
-                    <div clasName="dateSection">
-                        <label className ="dateLabel">Date:</label>
-                        <input
-                            className="dateInput"
-                            type="text"
-                            name="date"
-                            placeholder="mm/dd/yyyy"
-                            value={this.state.date}
-                            onChange={this.handleChange}     
-                        >
-                        </input>
-                    </div>
-                    <div className="TargetedSpeciesContainer">
-                        <label>Targeted Species: </label>
-                        <select
-                            className="targetedSpecies"
-                            name="targetedSpecies"
-                            onChange={this.handleChange}
-                            value={this.state.targetedSpecies}
-                        >
-                            {this.getFishOptions()}
-                        </select>
-                    </div>
-                    <div className="timeCont">
-                        <label className="timeLabel">Time:</label>
-                        <input
-                            className="timeInput"
-                            type="text"
-                            name="time"
-                            placeholder="6:00 am"
-                            onChange={this.handleChange}
-                            value={this.state.time}
-                        >
-                        </input>
-                    </div>
+                        <div clasName="dateSection">
+                            <label className ="dateLabel">Date:</label>
+                            <input
+                                className="dateInput"
+                                type="text"
+                                name="date"
+                                placeholder="mm/dd/yyyy"
+                                value={this.state.date}
+                                onChange={this.handleChange}     
+                            >
+                            </input>
+                        </div>
+                        <div className="TargetedSpeciesContainer">
+                            <label>Targeted Species: </label>
+                            <select
+                                className="targetedSpecies"
+                                name="targetedSpecies"
+                                onChange={this.handleChange}
+                                value={this.state.targetedSpecies}
+                            >
+                                {this.getFishOptions()}
+                            </select>
+                        </div>
+                        <div className="timeCont">
+                            <label className="timeLabel">Time:</label>
+                            <input
+                                className="timeInput"
+                                type="text"
+                                name="time"
+                                placeholder="6:00 am"
+                                onChange={this.handleChange}
+                                value={this.state.time}
+                            >
+                            </input>
+                        </div>
                     </div>
                     <div className="row2">
-                    <div className="length">
-                        <label>Hours Fished: </label>
-                        <input
-                            type="text"
-                            name="length"
-                            placeholder="0"
-                            value={this.state.length}
-                            onChange={this.handleChange}
-                        >
-                        </input>
-                    </div>
-                    <div className="temperature">
-                        <label>Temperature: </label> 
-                        <input 
-                            type="number"
-                            name="temperature"
-                            placeholder="Degreees Farenheidt"
-                            value={this.state.temperature}
-                            onChange={this.handleChange}
-                        >    
-                        </input>
-                    </div>
-                    <div className="weather">
-                        <label>Weather: </label>
-                        <input 
-                            type="text" 
-                            name="weather" 
-                            placeholder="Sunny, rainy, etc." 
-                            placeholderStyle={{fontSize: 12}}
-                            value={this.state.weather} 
-                            onChange={this.handleChange}
+                        <div className="length">
+                            <label>Hours Fished: </label>
+                            <input
+                                type="text"
+                                name="length"
+                                placeholder="0"
+                                value={this.state.length}
+                                onChange={this.handleChange}
                             >
-                        </input>
-                    </div>
-                    <div className="lureCont">
-                        <label>Best Lure:</label>
-                        <select 
-                            className="lureChoice" 
-                            name='bestLure' 
-                            value={this.state.lure} 
-                            onChange={this.handleChange}
-                        >
-                            {this.lureDropDown()}
-                        </select>
-                    </div>
+                            </input>
+                        </div>
+                        <div className="temperature">
+                            <label>Temperature: </label> 
+                            <input 
+                                type="number"
+                                name="temperature"
+                                placeholder="Degreees Farenheidt"
+                                value={this.state.temperature}
+                                onChange={this.handleChange}
+                            >    
+                            </input>
+                        </div>
+                        <div className="weather">
+                            <label>Weather: </label>
+                            <input 
+                                type="text" 
+                                name="weather" 
+                                placeholder="Sunny, rainy, etc." 
+                                placeholderStyle={{fontSize: 12}}
+                                value={this.state.weather} 
+                                onChange={this.handleChange}
+                            >
+                            </input>
+                        </div>
+                        <div className="lureCont">
+                            <label>Best Lure:</label>
+                            <select 
+                                className="lureChoice" 
+                                name='bestLure' 
+                                value={this.state.lure} 
+                                onChange={this.handleChange}
+                            >
+                                {this.lureDropDown()}
+                            </select>
+                        </div>
                     </div>
                     <div className="descriptionWrapper">
                         <p className="descriptionHeader">Description: </p>
