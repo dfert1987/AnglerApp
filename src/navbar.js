82e3613ec0d5b829b6453a7f229a63ff6f0b469e
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-05-18 14:30:38 -0600

    navbar style fixes

--- src/navbar.js
+++ src/navbar.js
@@ -1,46 +1,42 @@
 import React, {Component} from 'react';
 import logo from './images/logo.png';
-import './navbar.css'
+import './navbar.css';
 
+class Navbar extends Component {
+  state = {
+    isOpen: false,
+  };
+  handleClick = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
 
-class Navbar extends Component{
-
-    state={
-        isOpen:false
-    }
-    handleClick = () => {
-        this.setState({
-            isOpen:!this.state.isOpen,
-        })
-    }
-
-    render(){
-        return(
-        // <Router>
-            <nav>
-                <div className="logoBtn">
-                    <div className="logo">
-                        <a href="#"><img src={logo} 
-                        className="logo" alt=""/></a>
-                    </div>
-
-                    <div className="btn"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-           
-
-                <ul>
-\                    <li><a href="/home"> Home  </a></li>
-                    <li><a href="/tacklebox"> TackleBox  </a></li>
-                    <li><a href="/MapPage">  Map  </a></li>
-                    <li><a href="/fish">  Fish-Dex </a></li>
-                </ul>
-                </div>
-            </nav>
-        // </Router>
-        )
-    }
+  render() {
+    return (
+      <nav>
+        <div className='logoBtn'>
+          <div className='logo'>
+            <img src={logo} className='logo' alt='' />
+          </div>
+          </div>
+          <ul>
+            <li>
+              <a href='/home'> Home </a>
+            </li>
+            <li>
+              <a href='/tacklebox'> TackleBox </a>
+            </li>
+            <li>
+              <a href='/fish'> FishDex </a>
+            </li>
+            <li>
+              <a href='/MapPage'> Map </a>
+            </li>
+          </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
