3b0ba3efd9f9b854ac015e24d14a5dd22bb7c386
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-05-20 00:07:14 -0600

    fish-dex css and layout fixes

--- src/components/FishCard.js
+++ src/components/FishCard.js
@@ -1,73 +1,126 @@
-import React, { Component } from 'react';
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
-import PRForm from './PRForm.js';
+import React, {Component} from 'react';
+import Flippy, {FrontSide, BackSide} from 'react-flippy';
 
+class FishCard extends Component {
+  state = {
+    pr: this.props.oneFish.pr,
+  };
 
+  handleChange = (event) => {
+    console.log(this.props);
+    this.setState({[event.target.name]: event.target.value});
+  };
 
-class  FishCard extends Component {
-  
-    prForm = () => {
-        if(this.props.removeFromCaught) {
-            return(
-                <PRForm
-                    oneFish = {this.props.oneFish}
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.prBackend();
+    this.prFrontend(event);
+  };
+
+  prBackend = () => {
+    this.props.oneFish.pr = this.state.pr;
+    fetch(`http://localhost:3000/fish/${this.props.oneFish.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.props.oneFish),
+    });
+  };
+
+  prFrontend = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  showPrForm = () => {
+    if (this.props.caught === true) {
+      return (
+        <div className='pr-container'>
+          <form onSubmit={this.handleSubmit} className='addPRForm'>
+            <h4 className='pr-title'>BIGGEST CATCH (In.)</h4>
+            <input
+              className='inputPR'
+              type='text'
+              name='pr'
+              placeholder='0 in'
+              value={this.state.pr}
+              onChange={this.handleChange}
+            />
+            <input className='submitPR' type='submit' value='Submit Size' />
+          </form>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  showBiggestCatch = () => {
+    if (this.props.caught === true) {
+      return (
+        <div>
+          <h3 className='PR'>Biggest Catch: {this.state.pr} in. </h3>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  render() {
+    const handleClick = () => {
+      if (this.props.addToCaught) {
+        this.props.addToCaught(this.props.oneFish);
+        this.props.caughtBackend(this.props.oneFish);
+      } else {
+        this.props.removeFromCaught(this.props.oneFish);
+        this.props.removeFromCaughtBackend(this.props.oneFish);
+      }
+    };
+
+    return (
+      <div className='FishCardContainer'>
+        <Flippy
+          className='flippy'
+          flipOnHover={false}
+          flipOnClick={true}
+          flipDirection='horizontal'
+          ref={(r) => (this.flippy = r)}
+          style={{width: '100%', height: '100%'}}
+        >
+          <FrontSide>
+            <div className='Card-Front'>
+                <h3 className='FishName'>{this.props.oneFish.species}</h3>
+                <img
+                  className='FishPic'
+                  src={this.props.oneFish.image}
+                  alt={this.props.oneFish.name}
                 />
-            )
-        }
-    }
-
-  
-
-    render() {
-        const handleClick = () =>{
-            if(this.props.addToCaught){
-              this.props.addToCaught(this.props.oneFish)
-              this.props.caughtBackend(this.props.oneFish)
-            }
-            else {
-              this.props.removeFromCaught(this.props.oneFish)
-              this.props.removeFromCaughtBackend(this.props.oneFish)
-              }
-          }
-
-       
-        return (
-            <div className="FishCardContainer">
-                <Flippy className='flippy'
-                    flipOnHover={false} 
-                    flipOnClick={true} 
-                    flipDirection="horizontal"
-                    ref={(r) => this.flippy = r} 
-                    style={{ width: '98%', height: '100%' }} 
-                >         
-                    <FrontSide>
-                        <div className="Card-Front">
-                            <div className="Front-Content">
-                                <h3 className="FishName" >{this.props.oneFish.species}</h3>
-                                <img className="FishPic" src={this.props.oneFish.image}/>
-                                <div className="Button-Container">
-                                    <button className='Button-Flip'>SEE INFO</button>
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </FrontSide>
-                    <BackSide>
-                        <div className="Card-Back">
-                            <div className="Card-Back-Content">
-                                <h4>About:</h4>
-                                <p className="fishparagraph">{this.props.oneFish.description}</p>
-                            </div>
-                            <button className='Button-Flip-Back'>SEE IMAGE</button>
-                        </div>
-                    </BackSide>
-                </Flippy>
-                    {this.prForm()}
-                <button className='AddCaughtButton' onClick={handleClick}>TOGGLE CAUGHT</button>
-
+                <div className='Button-Container'>
+                  <button className='Button-Flip'>SEE INFO</button>
+                </div>
+                {this.showBiggestCatch()}
             </div>
-        )
-    }
+          </FrontSide>
+          <BackSide>
+            <div className='Card-Back'>
+              <div className='Card-Back-Content'>
+                <h4>About:</h4>
+                <p className='fishparagraph'>
+                  {this.props.oneFish.description}
+                </p>
+              </div>
+              <button className='Button-Flip-Back'>SEE IMAGE</button>
+            </div>
+          </BackSide>
+        </Flippy>
+        {this.showPrForm()}
+        <button className='AddCaughtButton' onClick={handleClick}>
+          TOGGLE CAUGHT
+        </button>
+      </div>
+    );
+  }
 }
 
 export default FishCard;
--- src/fish.js
+++ src/fish.js
@@ -1,122 +1,128 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navbar from './navbar.js';
 import FishCard from './components/FishCard.js';
-import './fish.css'
+import './fish.css';
 
 class Fish extends Component {
+  state = {
+    allFish: [],
+    caughtFish: [],
+  };
 
-    state = {
-        allFish: [],
-        caughtFish:[]  
+  componentDidMount() {
+    fetch('http://localhost:3000/fish')
+      .then((response) => response.json())
+      .then((result) => this.controllerFunction(result));
+  }
+
+  controllerFunction = (result) => {
+    this.setFish(result);
+  };
+
+  setFish = (result) => {
+    this.setState({allFish: result});
+    this.setCaughtFish(result);
+  };
+
+  setCaughtFish = () => {
+    const setCF = this.state.allFish.filter(
+      (oneFish) => oneFish.caught !== false
+    );
+    this.setState({
+      caughtFish: setCF,
+    });
+  };
+
+  showFish = () =>
+    this.state.allFish.map((oneFish) => {
+      return (
+        <FishCard
+          key={oneFish.id}
+          addToCaught={this.addToCaught}
+          caughtBackend={this.caughtBackend}
+          oneFish={oneFish}
+          caught={false}
+        />
+      );
+    });
+
+  addToCaught = (oneFish) => {
+    if (
+      !this.state.caughtFish.find((activeFish) => oneFish.id === activeFish.id)
+    ) {
+      this.setState({
+        caughtFish: [...this.state.caughtFish, oneFish],
+      });
     }
+  };
 
-    componentDidMount(){
-        fetch('http://localhost:3000/fish')
-            .then(response => response.json())
-            .then(result => this.controllerFunction(result)
-        )
-    }
+  caughtBackend = (oneFish) => {
+    if (oneFish.caught === false) {
+      oneFish.caught = true;
+    } else oneFish.caught = true;
+    fetch(`http://localhost:3000/fish/${oneFish.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(oneFish),
+    });
+  };
 
-    controllerFunction = (result) => {
-        this.setFish(result)
-    }
+  showCaught = () =>
+    this.state.caughtFish.map((oneFish) => {
+      return (
+        <FishCard
+          key={oneFish.id}
+          removeFromCaught={this.removeFromCaught}
+          removeFromCaughtBackend={this.removeFromCaughtBackend}
+          oneFish={oneFish}
+          caught={true}
+        />
+      );
+    });
 
-    setFish = (result) => {
-        this.setState(
-            {allFish: result}
-        )
-        this.setCaughtFish(result)
-    }
+  removeFromCaught = (oneFish) => {
+    const filtered = this.state.caughtFish.filter(
+      (activeFish) => activeFish.id !== oneFish.id
+    );
+    this.setState({caughtFish: filtered});
+  };
 
-    setCaughtFish = () => {
-        const setCF = this.state.allFish.filter(oneFish => oneFish.caught !== false)
-        this.setState({
-            caughtFish:setCF
-        })
-    }
+  removeFromCaughtBackend = (oneFish) => {
+    if (oneFish.caught === true) {
+      oneFish.caught = false;
+    } else oneFish.caught = false;
+    fetch(`http://localhost:3000/fish/${oneFish.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(oneFish),
+    });
+  };
 
-    showFish = () => this.state.allFish.map(oneFish => {
-        return <FishCard key={oneFish.id} 
-            addToCaught={this.addToCaught}
-            caughtBackend={this.caughtBackend} 
-            oneFish={oneFish}/> 
-    })
-
-    addToCaught= (oneFish) => {
-        if(!this.state.caughtFish.find(activeFish => oneFish.id === activeFish.id)){
-            this.setState({
-                caughtFish:[...this.state.caughtFish, oneFish]
-            })
-        }
-    }  
-
-    caughtBackend = (oneFish) => {
-        if (oneFish.caught === false) {
-            oneFish.caught = true
-        } else oneFish.caught = true 
-            fetch(`http://localhost:3000/fish/${oneFish.id}`, {
-                method: 'PATCH',
-                headers: {
-                "Content-Type": "application/json"
-                },
-                body: JSON.stringify(oneFish)
-                })
-            }
-
-    showCaught = () => this.state.caughtFish.map(oneFish => {
-        return <FishCard key={oneFish.id}
-            removeFromCaught={this.removeFromCaught} 
-            removeFromCaughtBackend={this.removeFromCaughtBackend}
-            oneFish={oneFish}/>
-    })
-
-    removeFromCaught= (oneFish) => {
-        const filtered = this.state.caughtFish.filter(activeFish => activeFish.id !== oneFish.id )
-            this.setState(
-                {caughtFish: filtered}
-            )
-        }
-
-    removeFromCaughtBackend =(oneFish) => {
-        if (oneFish.caught === true) {
-            oneFish.caught = false
-        } else oneFish.caught = false 
-            fetch(`http://localhost:3000/fish/${oneFish.id}`, {
-                method: 'PATCH',
-                headers: {
-                "Content-Type": "application/json"
-                },
-                body: JSON.stringify(oneFish)
-                })
-            }
-
-    render(){
-        return  (
-            <div className="main">
-                <div className="nav-bar">
-                    <Navbar />
-                </div>
-                <div>
-                    <h1>FISH-DEX</h1>
-                </div> 
-                <div className="all-fish">
-                    <div className="fishContainer">
-                        <h2 className='NotCaughtTitle'>ALL FISH</h2>
-                            <ul className="fish-list">
-                                {this.showFish()}
-                            </ul>
-                    </div>
-                    <div className="caught-fish">
-                        <div className="caught-fishContainer">
-                            <h2 className='CaughtTitle'>CAUGHT</h2>
-                            <ul className='caught-list'>
-                                {this.showCaught()}
-                            </ul>
-                        </div>
-                    </div> 
-                </div>
-            </div>
-            )
-        }
-    }
+  render() {
+    return (
+      <div className='main'>
+        <div className='nav-bar'>
+          <Navbar />
+        </div>
+        <div>
+          <h1>FISH-DEX</h1>
+        </div>
+        <div className='all-fish'>
+          <div className='fishContainer'>
+            <h2 className='NotCaughtTitle'>ALL FISH</h2>
+            <ul className='fish-list'>{this.showFish()}</ul>
+          </div>
+          <div className='caught-fishContainer'>
+            <h2 className='CaughtTitle'>CAUGHT</h2>
+            <ul className='caught-list'>{this.showCaught()}</ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default Fish;
