1f17a5188843e5463ff791c60bb356beb05e1063
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-05-20 22:17:43 -0600

    lure card css fixes

--- src/components/LureCard.js
+++ src/components/LureCard.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from 'react';
+import '../tacklebox.css';
 
-export default function LureCard({lure, addToTackleBox, removeFromTackleBox, favoriteBackend, unfavoriteBackend}){
-
-    const handleClick = () =>{
-      if(addToTackleBox){
-        addToTackleBox(lure)
-        favoriteBackend(lure)
-      }
-      else {
-        removeFromTackleBox(lure)
-        unfavoriteBackend(lure)
-        }
+export default function LureCard({
+  lure,
+  addToTackleBox,
+  removeFromTackleBox,
+  favoriteBackend,
+  unfavoriteBackend,
+}) {
+  const handleClick = () => {
+    if (addToTackleBox) {
+      addToTackleBox(lure);
+      favoriteBackend(lure);
+    } else {
+      removeFromTackleBox(lure);
+      unfavoriteBackend(lure);
     }
-    
-        return (
-            <li className="lure-card" >
-              <h3>{lure.name}</h3>
-              <img className ="lure-card-img" src={lure.image}/>
-              <h4>BRAND: {lure.brand}</h4>
-              <h4>TYPE: {lure.lureType}</h4>
-              <h4>COLOR: {lure.color}</h4>
-              <h4>SIZE: {lure.size}</h4>
-              <button className="Add-remove" onClick={handleClick}>Add/Remove</button>
-            </li>
-          )
-        }
+  };
+
+  return (
+    <li className='lure-card'>
+      <h3>{lure.name}</h3>
+      <img className='lure-card-img' src={lure.image} alt='lure' />
+      <h4 className='card-item-brand'>BRAND: {lure.brand}</h4>
+      <h4 className='card-item'>TYPE: {lure.lureType}</h4>
+      <h4 className='card-item'>COLOR: {lure.color}</h4>
+      <h4 className='card-item'>SIZE: {lure.size}</h4>
+      <button className='Add-remove' onClick={handleClick}>
+        Add/Remove
+      </button>
+    </li>
+  );
+}
