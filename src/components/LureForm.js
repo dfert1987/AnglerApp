f84f24b3b6e67c685c7de059b949bf54215d3607
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-05-20 21:40:47 -0600

    tackle box form css fixes

--- src/components/LureForm.js
+++ src/components/LureForm.js
@@ -1,65 +1,119 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
 export default class LureForm extends Component {
+  state = {
+    id: 6,
+    name: '',
+    image: '',
+    brand: '',
+    lureType: '',
+    color: '',
+    size: '',
+    favorited: false,
+  };
 
-    state = {
-        id: 6,
-        name: "",
-        image: "",
-        brand: "",
-        lureType: "",
-        color: "",
-        size: "",
-        favorited: false
-    }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]:event.target.value
-        })
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addLure(this.state);
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        this.props.addLure(this.state)
-    }
-
-    render(){
-        
-        return (
-            <form onSubmit={this.handleSubmit} className="addLure">
-                <h2 className="form-title">ADD NEW LURE</h2>
-                <label> NAME:  </label>
-                <input className="input" type="text" name='name' placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
-                <label>  IMAGE:  </label>
-                <input className="input" type="text" name='image' placeholder="Image URL" value={this.state.image} onChange={this.handleChange}/> <br/>
-                <label>  BRAND:  </label>
-                <input className="input" type="text" name='brand' placeholder="Brand" value={this.state.brand} onChange={this.handleChange} /> 
-                <label>  TYPE:  </label>
-                <select className="input" id="lure-types" name='lureType' value={this.state.lureType} onChange={this.handleChange}>
-                    <option value = "crank">CRANK</option>
-                    <option value = "popper">POPPER</option>
-                    <option value = "fly">FLY</option>
-                    <option value = "swim bait">SWIM BAIT</option>
-                    <option value = "spoon">SPOON</option>
-                    <option value = "jig">JIG</option>
-                    <option value = "jerk">JERK</option>
-                    <option value = "spinner">SPINNER</option>
-                    <option value = "soft plastic">SOFT PLASTIC</option>
-                    <option value = "topwater">TOPWATER</option>
-                </select><br/>
-                <label>  COLOR:  </label>
-                <input className="input" type="text" name='color' placeholder="Color" value={this.state.color} onChange={this.handleChange}/>
-                <label>  SIZE:  </label>
-                <select className="input" id="lure-size" name='size' value={this.state.size} onChange={this.handleChange}>
-                    <option value = "S">SMALL</option>
-                    <option value = "M">MEDIUM</option>
-                    <option value = "L">LARGE</option>
-                </select><br/>
-                <input type="hidden" name="favorited" value = {this.state.favorited}/>
-                <input type="hidden" name="id" value = {this.state.id ++}/>
-                <input className="submit" type="submit" value="Add Lure"/>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className='addLure'>
+        <h2 className='form-title'>ADD NEW LURE</h2>
+        <div className='first-row'>
+          <div className='name-container'>
+            <label> NAME: </label>
+            <input
+              className='name-input'
+              type='text'
+              name='name'
+              placeholder='Name'
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='image-container'>
+            <label> IMAGE: </label>
+            <input
+              className='image-input'
+              type='text'
+              name='image'
+              placeholder='Image URL'
+              value={this.state.image}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='brand-container'>
+            <label> BRAND: </label>
+            <input
+              className='brand-input'
+              type='text'
+              name='brand'
+              placeholder='Brand'
+              value={this.state.brand}
+              onChange={this.handleChange}
+            />
+          </div>
+        </div>
+        <div className='row-2'>
+          <div className='type-container'>
+            <label> TYPE: </label>
+            <select
+              className='type-input'
+              id='lure-types'
+              name='lureType'
+              value={this.state.lureType}
+              onChange={this.handleChange}
+            >
+              <option value='crank'>CRANK</option>
+              <option value='popper'>POPPER</option>
+              <option value='fly'>FLY</option>
+              <option value='swim bait'>SWIM BAIT</option>
+              <option value='spoon'>SPOON</option>
+              <option value='jig'>JIG</option>
+              <option value='jerk'>JERK</option>
+              <option value='spinner'>SPINNER</option>
+              <option value='soft plastic'>SOFT PLASTIC</option>
+              <option value='topwater'>TOPWATER</option>
+            </select>
+          </div>
+          <div className='color-container'>
+            <label> COLOR: </label>
+            <input
+              className='color-input'
+              type='text'
+              name='color'
+              placeholder='Color'
+              value={this.state.color}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='size-container'>
+            <label> SIZE: </label>
+            <select
+              className='size-input'
+              id='lure-size'
+              name='size'
+              value={this.state.size}
+              onChange={this.handleChange}
+            >
+              <option value='S'>SMALL</option>
+              <option value='M'>MEDIUM</option>
+              <option value='L'>LARGE</option>
+            </select>
+          </div>
+        </div>
+        <input type='hidden' name='favorited' value={this.state.favorited} />
+        <input type='hidden' name='id' value={this.state.id++} />
+        <input className='submit' type='submit' value='Add Lure' />
+      </form>
+    );
+  }
 }
--- src/tackle box.js
+++ src/tackle box.js
@@ -1,139 +1,143 @@
-import React, { Component } from 'react';
-import './tacklebox.css';
+import React, {Component} from 'react';
 import LureCard from './components/LureCard';
 import LureForm from './components/LureForm';
-import Navbar from './navbar.js'
+import Navbar from './navbar.js';
+import './tacklebox.css';
 
 class TackleBox extends Component {
+  state = {
+    lures: [],
+    tacklebox: [],
+  };
 
-    state = {
-        lures: [],
-        tacklebox: []   
-      }
+  componentDidMount() {
+    fetch('http://localhost:3000/lures')
+      .then((response) => response.json())
+      .then((result) => this.controllerFunction(result));
+  }
 
-    componentDidMount(){
-        fetch('http://localhost:3000/lures')
-            .then(response => response.json())
-            .then(result => this.controllerFunction(result)
-        )
+  controllerFunction = (result) => {
+    this.setLures(result);
+  };
+
+  setLures = (result) => {
+    this.setState({lures: result});
+    this.setTackleBox(result);
+  };
+
+  setTackleBox = () => {
+    const setTB = this.state.lures.filter((lure) => lure.favorited !== false);
+    this.setState({
+      tacklebox: setTB,
+    });
+  };
+
+  showLures = () =>
+    this.state.lures.map((lure) => {
+      return (
+        <LureCard
+          key={lure.id}
+          addToTackleBox={this.addToTackleBox}
+          favoriteBackend={this.tackleBoxBackend}
+          lure={lure}
+        />
+      );
+    });
+
+  showTackleBox = () =>
+    this.state.tacklebox.map((lure) => {
+      return (
+        <LureCard
+          key={lure.id}
+          removeFromTackleBox={this.removeFromTackleBox}
+          unfavoriteBackend={this.unfavoriteBackend}
+          lure={lure}
+        />
+      );
+    });
+
+  addToTackleBox = (lure) => {
+    if (!this.state.tacklebox.find((activeLure) => lure.id === activeLure.id)) {
+      this.setState({
+        tacklebox: [...this.state.tacklebox, lure],
+      });
     }
+  };
 
-    controllerFunction = (result) => {
-        this.setLures(result)
-    }
+  tackleBoxBackend = (lure) => {
+    if (lure.favorited === false) {
+      lure.favorited = true;
+    } else lure.favorited = true;
+    fetch(`http://localhost:3000/lures/${lure.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(lure),
+    });
+  };
 
-    setLures =(result) => {
-        this.setState(
-            { lures: result }
-          )
-          this.setTackleBox(result)
-        }
+  removeFromTackleBox = (lure) => {
+    const filtered = this.state.tacklebox.filter(
+      (activeLure) => activeLure.id !== lure.id
+    );
+    this.setState({tacklebox: filtered});
+  };
 
-    setTackleBox =() => {
-        const setTB = this.state.lures.filter(lure => lure.favorited !== false)
-        this.setState({
-            tacklebox:setTB
-        })
-    }
-    
-    showLures = () => this.state.lures.map(lure => {
-        return <LureCard key={lure.id} 
-            addToTackleBox={this.addToTackleBox}
-            favoriteBackend={this.tackleBoxBackend} 
-            lure={lure}/> 
-    })
-    
-    showTackleBox = () => this.state.tacklebox.map(lure => {
-        return <LureCard key={lure.id} 
-            removeFromTackleBox={this.removeFromTackleBox} 
-            unfavoriteBackend={this.unfavoriteBackend}
-            lure={lure}/>
-    })
+  unfavoriteBackend = (lure) => {
+    if (lure.favorited === true) {
+      lure.favorited = false;
+    } else lure.favorited = false;
+    fetch(`http://localhost:3000/lures/${lure.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(lure),
+    });
+  };
 
-    addToTackleBox = (lure) => {
-        if(!this.state.tacklebox.find(activeLure => lure.id === activeLure.id)){
-            this.setState({
-                tacklebox:[...this.state.tacklebox, lure]
-            })
-        }
-    }   
+  addLure = (newLure) => {
+    this.setState({
+      lures: [...this.state.lures, newLure],
+    });
+    const lure = {
+      lure: newLure,
+    };
+    fetch('http://localhost:3000/lures', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(lure),
+    });
+  };
 
-    tackleBoxBackend = (lure) => {
-        if (lure.favorited === false) {
-            lure.favorited = true
-        } else lure.favorited = true 
-            fetch(`http://localhost:3000/lures/${lure.id}`, {
-                method: 'PATCH',
-                headers: {
-                "Content-Type": "application/json"
-                },
-                body: JSON.stringify(lure)
-            })
-        }
-
-    removeFromTackleBox = (lure) => {
-        const filtered = this.state.tacklebox.filter(activeLure => activeLure.id !== lure.id )
-            this.setState(
-                {tacklebox: filtered}
-            )
-        }
-
-    unfavoriteBackend =(lure) => {
-        if (lure.favorited === true) {
-            lure.favorited = false
-        } else lure.favorited = false 
-            fetch(`http://localhost:3000/lures/${lure.id}`, {
-                method: 'PATCH',
-                headers: {
-                "Content-Type": "application/json"
-                },
-                body: JSON.stringify(lure)
-            })
-        }
-
-    addLure = (newLure) => {
-        this.setState({
-            lures: [...this.state.lures, newLure]
-        })
-        const lure = {
-            lure: newLure
-        }
-        fetch('http://localhost:3000/lures', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-           body: JSON.stringify(lure) 
-        })
-    }
-    
-    render(){
-        return  (
-            <div className="main-tb">
-                <div className="nav-bar">
-                    <Navbar />
-                </div>
-                <div className="Title-Container">
-                    <h1 className="TB-title">TACKLE BOX</h1>
-                    <h3 className="TB-subtitle"> - Customize Your Tackle Box -</h3>
-                </div>
-                <div className="your-tacklebox">
-                    <h3 className="tacklebox-header">CURRENT TACKLE BOX</h3>
-                    <ul className="current-tacklebox">
-                        {this.showTackleBox()}
-                    </ul>
-                </div>
-                <div className="form container">
-                    <LureForm addLure={this.addLure}/>
-                </div>
-                <div className="all-lures">
-                    <h3 className="all-lures-header">LURE OPTIONS</h3>
-                    <ul className="lure-list">
-                        {this.showLures()}
-                    </ul>
-                </div>
-            </div>
-            )
-        }
-    }
+  render() {
+    return (
+      <div className='main-tb'>
+        <div className='nav-bar'>
+          <Navbar />
+        </div>
+        <div className='body-container'>
+          <div className='Title-Container'>
+            <h1 className='TB-title'>TACKLE BOX</h1>
+            <h3 className='TB-subtitle'> - Customize Your Tackle Box -</h3>
+          </div>
+          <div className='your-tacklebox'>
+            <h3 className='tacklebox-header'>CURRENT TACKLE BOX</h3>
+            <ul className='current-tacklebox'>{this.showTackleBox()}</ul>
+          </div>
+          <div className='form-container'>
+            <LureForm addLure={this.addLure} />
+          </div>
+          <div className='all-lures'>
+            <h3 className='all-lures-header'>LURE OPTIONS</h3>
+            <ul className='lure-list'>{this.showLures()}</ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default TackleBox;
